ad8e09c226dfefb759bff9063dd24cc41482e2bf
Zhan Fa <1372306571@qq.com>
2024-05-08 20:12:53 +0800

    origin

---
+++ code/test_LA.py
@@ -0,0 +1,56 @@
+import os
+import argparse
+import torch
+import pdb
+
+from networks.net_factory import net_factory
+from utils.test_3d_patch import test_all_case
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--root_path', type=str, default='/data/byh_data/SSNet_data/LA/', help='Name of Experiment')
+parser.add_argument('--exp', type=str,  default='BCP', help='exp_name')
+parser.add_argument('--model', type=str,  default='VNet', help='model_name')
+parser.add_argument('--gpu', type=str,  default='0', help='GPU to use')
+parser.add_argument('--detail', type=int,  default=1, help='print metrics for every samples?')
+parser.add_argument('--nms', type=int, default=1, help='apply NMS post-procssing?')
+parser.add_argument('--labelnum', type=int, default=8, help='labeled data')
+parser.add_argument('--stage_name',type=str, default='self_train', help='self_train or pre_train')
+
+FLAGS = parser.parse_args()
+
+os.environ['CUDA_VISIBLE_DEVICES'] = FLAGS.gpu
+snapshot_path = "./model/BCP/LA_{}_{}_labeled/{}".format(FLAGS.exp, FLAGS.labelnum, FLAGS.stage_name)
+test_save_path = "./model/BCP/LA_{}_{}_labeled/{}_predictions/".format(FLAGS.exp, FLAGS.labelnum, FLAGS.model)
+num_classes = 2
+
+if not os.path.exists(test_save_path):
+    os.makedirs(test_save_path)
+print(test_save_path)
+with open('/data/fazhan/BCP-main/code/data/2018LA_Seg_Training Set/test.list', 'r') as f:
+    image_list = f.readlines()
+image_list = ["/data/fazhan/BCP-main/code/data/2018LA_Seg_Training Set/" + item.replace('\n', '') + "/mri_norm2.h5" for item in image_list]
+
+
+def test_calculate_metric():
+    model = net_factory(net_type=FLAGS.model, in_chns=1, class_num=num_classes, mode="test")
+    save_model_path = os.path.join(snapshot_path, '{}_best_model.pth'.format(FLAGS.model))
+    save_model_path="/data/fazhan/BCP-main/code/model/BCP/LA_BCP_4_labeled/self_train_ex_CCM/iter_9400_dice_0.8956.pth"
+    model.load_state_dict(torch.load(save_model_path))
+    print("init weight from {}".format(save_model_path))
+
+    model.eval()
+
+    avg_metric = test_all_case(model, image_list, num_classes=num_classes,
+                           patch_size=(112, 112, 80), stride_xy=18, stride_z=4,
+                           save_result=True, test_save_path=test_save_path,
+                           metric_detail=FLAGS.detail, nms=FLAGS.nms)
+
+
+    return avg_metric
+
+
+if __name__ == '__main__':
+    metric = test_calculate_metric()
+    print(metric)
+
+# python test_LA.py --model 0214_re01 --gpu 0
